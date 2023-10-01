444dcf2185e2cef003674cc8108e4cc7dadc719d
Ali Parvizi <140019647+Alienvec@users.noreply.github.com>
2023-10-01 20:20:27 +0200

    Add files via upload

---
+++ Week 4/helpers.c
@@ -0,0 +1,97 @@
+#include "bmp.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = (int) ((image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3.0 + 0.5);
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+        }
+    }
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = fmin(255, (int) (0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue + 0.5));
+            image[i][j].rgbtGreen = fmin(255, (int) (0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue + 0.5));
+            image[i][j].rgbtBlue = fmin(255, (int) (0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue + 0.5));
+        }
+    }
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a temporary image to store the blurred values
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            float counter = 0.00;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    // Check if the current pixel is within bounds
+                    if (currentX >= 0 && currentX < height && currentY >= 0 && currentY < width)
+                    {
+                        totalRed += image[currentX][currentY].rgbtRed;
+                        totalGreen += image[currentX][currentY].rgbtGreen;
+                        totalBlue += image[currentX][currentY].rgbtBlue;
+                        counter++;
+                    }
+                }
+            }
+
+            temp[i][j].rgbtRed = round(totalRed / counter);
+            temp[i][j].rgbtGreen = round(totalGreen / counter);
+            temp[i][j].rgbtBlue = round(totalBlue / counter);
+        }
+    }
+
+    // Copy the blurred image back to the original image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+}
